e1689dc835ab911c172b3d77660b8e18cd149237
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2023-03-07 14:46:52 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Meals from './pages/Meals';
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../App.css';
 import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
