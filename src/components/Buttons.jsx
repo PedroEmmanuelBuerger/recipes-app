19d5614602d58ebf44700d56d175524306e8cdb0
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2023-03-08 16:38:26 -0300

    wip req 27

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import copy from 'clipboard-copy';
+import shareIcon from '../images/shareIcon.svg';
 
 export default function Buttons() {
   const history = useHistory();
   const { location: { pathname } } = history;
+
+  const [share, setShare] = useState(false);
 
   const starRecipeBtn = () => {
     const id = pathname.split('/')[2];
@@ -13,8 +17,14 @@ export default function Buttons() {
     return history.push(`/drinks/${id}/in-progress`);
   };
 
+  const shareRecipeBtn = () => {
+    copy(`http://localhost:3000${pathname}`);
+    setShare(true);
+  };
+
   return (
     <div>
+      {share && <h2>Link copied!!</h2>}
       <button
         type="button"
         data-testid="start-recipe-btn"
@@ -28,8 +38,8 @@ export default function Buttons() {
       >
         Continue Recipe
       </button>
-      <button data-testid="share-btn">
-        share recipe
+      <button data-testid="share-btn" onClick={ shareRecipeBtn }>
+        <img src={ shareIcon } alt="share" />
       </button>
       <button data-testid="favorite-btn">
         favorite recipe
