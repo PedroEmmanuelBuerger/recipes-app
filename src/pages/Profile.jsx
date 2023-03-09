9a74728d007b1459a27b2179b6ca380b4c645350
Maisa Moraes <maisa.moraes@outlook.com>
2023-03-09 15:42:00 -0300

    Implementa a página de Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const history = useHistory();
   return (
     <div>
       <Header />
+
+      <span data-testid="profile-email">
+        {/* Falta pegar e-mail do estado que vem da página Login */}
+      </span>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => history.push('/') }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
