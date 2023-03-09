3d376a0d06c920438f25bffab7bfb7cc26aeb1b9
Pedro Emmanuel <113057901+PedroEmmanuelBuerger@users.noreply.github.com>
2023-03-09 18:18:46 -0300

    resolvendo linter

--- src/context/AppReceitasProvider.js
+++ src/context/AppReceitasProvider.js
@@ -1,7 +1,6 @@
-import React, { useState, useMemo,  useEffect, useCallback } from 'react';
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { fetchApiComidas, fetchApiBebidas } from '../helpers/fetchApi';
 import AppReceitasContext from './AppReceitasContext';
 
 function AppReceitasProvider({ children }) {
@@ -71,14 +70,12 @@ function AppReceitasProvider({ children }) {
     setSelected,
     buscaPorBebida,
     setBuscaPorBebida,
-    // LOGIN
     email,
     setEmail,
     senha,
     setSenha,
     habilitarDesabilitar,
     setHabilitarDesabilitar,
-    // LOGIN
   }), [
     buscaPorComida,
     setBuscaPorComida,
@@ -90,7 +87,6 @@ function AppReceitasProvider({ children }) {
     setSearchBarInput,
     textInput,
     setTextInput,
-  }), [
     fetchAPI,
     selected,
     buscaPorBebida,
