e69c9d2d1af4ce5d7256ba0d584029db64267a87
Tanara Silveira <tanahra@gmail.com>
2023-03-09 17:51:04 -0300

    Req 2-6 - implementa a pagina de LOGIN

--- src/context/AppReceitasProvider.js
+++ src/context/AppReceitasProvider.js
@@ -1,26 +1,55 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { fetchApiComidas, fetchApiBebidas } from '../helpers/fetchApi';
+// import { fetchApiComidas, fetchApiBebidas } from '../helpers/fetchApi';
 import AppReceitasContext from './AppReceitasContext';
 // Função para chamada da Api de comidas e bebidas. Ver endpoint
 function AppReceitasProvider({ children }) {
   const [buscaPorComida, setBuscaPorComida] = useState('milk');
   const [buscaPorBebida, setBuscaPorBebida] = useState('');
-  useEffect(() => {
-    fetchApiComidas(buscaPorComida);
-    fetchApiBebidas(buscaPorBebida);
-  }, [buscaPorComida, buscaPorBebida]);
+
+  // Implementando lógica de armazenamento de e-mail e senha(LOGIN)
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  // Implementando lógica de habilitação do botão Entrar(LOGIN)
+  const [habilitarDesabilitar, setHabilitarDesabilitar] = useState(true);
+  const verificaEmaileSenha = () => {
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+    const verifyEmail = regex.test(email);
+    const minCharacters = 6;
+    if (verifyEmail && senha.length > minCharacters) {
+      setHabilitarDesabilitar(false);
+    } else {
+      setHabilitarDesabilitar(true);
+    }
+    return habilitarDesabilitar;
+  };
+  useEffect(() => { verificaEmaileSenha(); }, [email, senha]);
 
   const context = useMemo(() => ({
     buscaPorComida,
     setBuscaPorComida,
     buscaPorBebida,
     setBuscaPorBebida,
+    // LOGIN
+    email,
+    setEmail,
+    senha,
+    setSenha,
+    habilitarDesabilitar,
+    setHabilitarDesabilitar,
+    // LOGIN
   }), [
     buscaPorComida,
     setBuscaPorComida,
     buscaPorBebida,
     setBuscaPorBebida,
+    email,
+    setEmail,
+    senha,
+    setSenha,
+    habilitarDesabilitar,
+    setHabilitarDesabilitar,
   ]);
 
   return (
